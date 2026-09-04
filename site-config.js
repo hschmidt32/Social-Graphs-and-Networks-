741415960d8560aa0d0c2754e6784ee9a09a0e73
johnsmith376 <hschmidt0032@aol.com>
2026-09-04 13:37:51 +0200

    names

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 window.SITE = {
   groupName: "Graph Verse",
-  members: ["Nicole Stepanenko", "Harrison Schmidt", "Member 3"],
+  members: ["Nicole Stepanenko", "Harrison Schmidt"],
   course: "02805 Social Graphs and Interactions"
 };
